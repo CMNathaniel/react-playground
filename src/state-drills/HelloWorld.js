b4d1e1cf0a00284331ed35b71f3f4b5fa6607513
Christine Nathaniel <christine@7geese.com>
2020-04-10 13:18:17 -0700

    adding Hello World drill

---
+++ src/state-drills/HelloWorld.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class HelloWorld extends Component {
+  state = {
+    who: "World"
+  };
+
+  render() {
+    return (
+      <div className="HelloWorld">
+        <p>Hello, {this.state.who}!</p>
+        <button onClick={() => this.setState({ who: "World" })}>World</button>
+        <br />
+        <button onClick={() => this.setState({ who: "Friend" })}>Friend</button>
+        <br />
+        <button onClick={() => this.setState({ who: "React" })}>React</button>
+      </div>
+    );
+  }
+}
